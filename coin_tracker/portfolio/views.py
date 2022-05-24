1e8430c69339107bde6dc97600f64ecd36fd2cfa
Aleksandr Kozhushkevich <kazhuha@gmail.com>
2022-05-24 23:33:20 +0300

    Добавил темплейты для страниц: index, coin, transactions

--- coin_tracker/portfolio/views.py
+++ coin_tracker/portfolio/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    template = 'portfolio/index.html'
+    return render(request, template)
+
+def transactions(request):
+    return HttpResponse('Страница со всеми транзакциями')
+
+def coin(request, slug):
+    return HttpResponse('Страница конкретной монеты')
